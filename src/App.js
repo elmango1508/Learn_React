609ce7034ba37c5e2b6297e6c6000efd4f97940d
tuan <tuanpham150801@gmail.com>
2022-10-04 00:03:42 +0700

    React State

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function ButtonPlus() {
 export default function App() {
   const [counter, setCounter] = useState(1);
   const [input, setInput] = useState("hi");
+  const [fname, setFname] = useState();
+  const [lname, setLname] = useState();
+  const [isSubmit, setIsSubmit] = useState(false);
   const cong = () => {
     setCounter(counter +1)
   }
@@ -24,9 +27,33 @@ export default function App() {
       console.log("Changing", event.target.value)
       setInput(event.target.value)
   }
+  const click = () => {    
+    alert(input)
+  }
+  const fName = (event) => {
+    setFname(event.target.value)
+  }
+  const lName = (event) => {
+    setLname(event.target.value)
+  }
+  const submit = () => {
+    setIsSubmit(true)
+  }
+  const showName = () => {
+    if(isSubmit == true){
+      return <p>My name is {fname} {lname}</p>
+    }
+    return<p>No name</p>
+  }
   return(
     <div>
-      <input value={input} onChange={handleChange}/>
+      <input value={input} onChange={handleChange}/> <button onClick={click}>Click</button>
+      <br></br>
+      FisrtName<input value={fname} onChange={fName}/>
+      LastName<input value={lname} onChange={lName}/>
+      <button onClick={submit}>Submit</button>
+      <br></br>
+      {showName()}
       <h2>Counter {counter}</h2>
       <button onClick={cong}>+</button>
       <button onClick={tru}>-</button>
